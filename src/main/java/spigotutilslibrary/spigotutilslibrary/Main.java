d897f5a0b16c303e81f314280985497d98dc41d5
SHHIKA <77447533+SHHIKA@users.noreply.github.com>
2021-08-27 19:05:15 +0900

    Initial commit

---
+++ src/main/java/spigotutilslibrary/spigotutilslibrary/Main.java
@@ -0,0 +1,23 @@
+package spigotutilslibrary.spigotutilslibrary;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    private static JavaPlugin plugin;
+
+    @Override
+    public void onEnable() {
+        plugin = this;
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    public static JavaPlugin getPlugin(){
+        return plugin;
+    }
+}
